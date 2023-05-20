5e073aacf6cb831de71414e9b0b0d4f793c99a8f
cankutay.ucar <cankutay.ucar@commercelab.com.tr>
2023-05-20 17:06:35 +0300

    ocelot api

---
+++ Gateways/CuMicroservice.Gateway/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuMicroservice.Gateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", opt =>
+            {
+                opt.Authority = Configuration["IdentityServerUrl"];
+                opt.Audience = "resource_gateway";
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false
+                };
+            });
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            await app.UseOcelot();
+        }
+    }
+}
--- Services/Catalog/CuMicroservice.Services.Catalog/Program.cs
+++ Services/Catalog/CuMicroservice.Services.Catalog/Program.cs
@@ -1,5 +1,7 @@
+using CuMicroservice.Services.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,16 @@ namespace CuMicroservice.Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+            var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+            if (!categoryService.GetAllAsync().Result.Data.Any())
+            {
+                categoryService.CreateAsync(new Dtos.CategoryDto { Name = "Asp.Net Core" }).Wait();
+                categoryService.CreateAsync(new Dtos.CategoryDto { Name = "Mssql Server" }).Wait();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
